5ff72581686c472e0a769cc5170086fbdc55bfc5
sayan <sayanadh123@gmail.com>
2025-06-29 23:28:01 +0530

    Week 2 Mandatory Exercises Corrected.

--- Week-2/PL-SQL/Code/is_vip.sql
+++
@@ -1,8 +0,0 @@
-BEGIN
-    FOR cust IN (SELECT CustomerID FROM Customers WHERE Balance > 10000) LOOP
-        UPDATE Customers
-        SET IsVIP = TRUE
-        WHERE CustomerID = cust.CustomerID;
-    END LOOP;
-END;
-/
